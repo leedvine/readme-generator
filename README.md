2c5e7b88f97495c1dae4a1d1d8ca59c3b0e98ccd
Lee Vine <leedvine@gmail.com>
2023-02-16 22:19:48 +0000

    The creation of the README file and the initial title is automated based on the users input - basic workings only

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# readme-generator
+
+
+# README Generator
+## Overview
+Create a command-line application that accepts user input. When a user is prompted for information about the application repository then a high-quality, professional README.md should be created. The user will be asked specific questions in different formats using "Inquirer" to determine the make up of the RADME file. 
+
--- index.mjs
+++ index.mjs
@@ -1,2 +1,30 @@
 import inquirer from 'inquirer';
-import fs from "fs/promises"
+
+import fs from 'fs/promises'
+
+
+let {title, overview} = await inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: "What's the readme title?",
+        },
+        {
+            type: 'input',
+            name: 'overview',
+            message: "Give brief overview of the project. Describe the overall premise and the main individual elements required to achieve project goals.",
+        },
+    ])
+
+// console.log(first_name, last_name);
+
+let readmeData = `
+
+# ${title}
+## Overview
+${overview}
+
+`
+
+await fs.writeFile('README.md', readmeData);
